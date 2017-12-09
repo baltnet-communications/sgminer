c9fb646f8d3ccca7fa99772be666c74a98ffb18d
The Ranger <ranger@risk.ee>
2017-12-09 17:38:58 +0200

    Initial version of Debian packaging rules

---
+++ debian/sgminer.default.ex
@@ -0,0 +1,10 @@
+# Defaults for sgminer initscript
+# sourced by /etc/init.d/sgminer
+# installed at /etc/default/sgminer by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
